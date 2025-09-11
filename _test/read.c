abb0b8934c610135148c523aa797b647917e9018
matth <matthewjcrosby1@gmail.com>
2025-09-11 08:18:05 +0700

    test

--- _test/read.c
+++ _test/read.c
@@ -1,14 +1,45 @@
 
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <malloc.h>
 
+enum ShapeKind { CIRCLE, RECTANGLE };
+
+struct TaggedShape {
+    enum ShapeKind kind;
+    union {
+        struct { 
+            double radius; 
+        } circle;
+        struct {
+            double w;
+            double h;
+        } rectange;
+    };
+};
+
+double area(struct TaggedShape s) {
+    return s.kind == CIRCLE
+        ? 3.14 * s.circle.radius * s.circle.radius
+        : s.rectange.w * s.rectange.h;
+};
+ 
 int main () {
-    freopen("operations.txt", "r+", stdin);
 
-    freopen("result.txt", "w", stdout);
+    // Utilizing fread, open a file called ./input.text, and read from it in 4096 chunks, 
+    // using fwrite to write the output into stdout.
+    FILE *fp = fopen("./input.txt", "r");
 
-    int a;
-    int b;
-    scanf("%d %d", &a, &b);
+    const int CHUNK_SIZE = 4096;
+    char *buff = malloc(CHUNK_SIZE);
 
-    printf("%d", a+b);
+    size_t nread;
+
+    while ((nread = fread(buff, 1, CHUNK_SIZE, fp)) > 0) {
+        fwrite(buff, 1, nread, stdout);
+    }
+
+    return 0;
+
 }
