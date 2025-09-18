aec9e88a90a8335d59a7211bb7f735b7c9a8bbe8
matth <matthewjcrosby1@gmail.com>
2025-09-18 08:15:59 +0700

    meee

--- _test/main.cc
+++ _test/main.cc
@@ -3,13 +3,19 @@
 #include <iostream>
 
 struct Foo {
-    Foo(int x) { std::cout << "Foo constructor ran"; }
-    ~Foo() { std::cout << "Foo destructor ran"; }
+    Foo(int x) {
+        std::cout << "Constructor";
+    }
+    ~Foo() {
+        std::cout << "Destructor222";
+    }
 };
 
 int main() {
-    void* buf = std::malloc(sizeof(Foo));
-    Foo* f = new (buf) Foo(10); // construct Foo at buf
-    f->~Foo();                 // must call destructor manually
-    std::free(buf);            // and free memory manually
+  void* buf = std::malloc(sizeof(Foo));
+  
+  Foo* f = new (buf) Foo(10);
+
+  f->~Foo();
+  std:free(buf);
 }
