e51d158dfb0fb0c7f053cce22bc57f15669e2a1a
matth <matthewjcrosby1@gmail.com>
2025-09-23 09:26:00 +0700

    test

--- _test/str/main.c
+++ _test/str/main.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 
-void test (int n, int (*nums)[n]) {
+void print_nums (int n, int *nums) {
     
     for (int i = 0; i < n; i++) {
-        printf("%d", *nums[i]);
+        printf("%d\n", nums[i]);
     }
 }
 
@@ -14,8 +14,17 @@ int main (int argc, char *argv[]) {
 
     srand(time(NULL));
 
-    int max = 100;
-    int min = 50;
-    printf("%d", min + rand() % (max - min + 1));
+    int *buf = malloc(1024 * sizeof(*buf));
+
+    if (!buf) {
+        perror("malloc");
+        return 1;
+    }
+
+    for (int i = 0; i < 1024; i++) {
+        buf[i] = rand();
+    }
+
+    print_nums(1024, buf);
 
 }
